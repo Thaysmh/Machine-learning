5e393c3a3823b8c4895e1c2db30fe71da8a95d95
thays <thays.nave@gmail.com>
2026-07-03 11:51:28 -0300

    Começando novo exercício de regressão simples.

---
+++ regression_2.py
@@ -0,0 +1,8 @@
+#Exercício 1: Previsão de Vendas por Investimento em Marketing (Regressão Linear Simples)
+#Contexto: Você quer prever as vendas de um produto (em milhares de unidades) com base no valor investido em anúncios no Instagram (em milhares de reais).
+
+#X (Input): Investimento em marketing (R$)
+
+#Y (Output): Vendas feitas (Unidades)
+
+#Desafio: Treine o modelo e preveja quantas vendas serão feitas com um investimento de R$ 45,00.
