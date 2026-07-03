6ef3158fca146079de3326d690533fa5f39bea83
thays <thays.nave@gmail.com>
2026-07-03 12:01:41 -0300

    Finalizando exercício de machine learning.

--- regression_2.py
+++ regression_2.py
@@ -6,3 +6,38 @@
 #Y (Output): Vendas feitas (Unidades)
 
 #Desafio: Treine o modelo e preveja quantas vendas serão feitas com um investimento de R$ 45,00.
+
+from sklearn.linear_model import LinearRegression
+
+x = [
+  [45],
+  [45],
+  [45],
+  [35],
+  [35],
+  [42],
+  [42],
+  [45]
+]
+
+y = [
+  1,
+  1,
+  2,
+  0,
+  1,
+  1,
+  2,
+  3
+]
+
+model = LinearRegression() # Executa a instância da classe LinearRegression
+
+model.fit(x, y)
+
+print("Coeficiente angular da reta:", model.coef_[0]) # Coeficiente angular da reta
+print("Coeficiente linear", model.intercept_) # Coeficiente linear da reta
+
+previsao = model.predict([[45]])
+
+print(previsao)
