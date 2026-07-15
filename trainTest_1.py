de1bc4960ef350b74f8c209ef9a3f3739520e020
thays <thays.nave@gmail.com>
2026-07-15 16:47:58 -0300

    Criando novo exercício.

---
+++ trainTest_1.py
@@ -0,0 +1,9 @@
+# Sua missão:
+
+# dividir os dados em 80% treino e 20% teste;
+# treinar apenas com os dados de treino;
+# prever os valores do conjunto de teste;
+# imprimir:
+# x_test
+# y_test
+# predições
